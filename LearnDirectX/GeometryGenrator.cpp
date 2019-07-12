4ea14fd40e63b2956037df52724abc3ac4f96dd1
tsxy2007 <z.tk@163.com>
2019-07-12 09:29:34 +0800

    材质

---
+++ LearnDirectX/GeometryGenrator.cpp
@@ -0,0 +1,136 @@
+#include "GeometryGenrator.h"
+#include <algorithm>
+
+using namespace DirectX;
+using MeshData = GeometryGenerator::MeshData;
+using Vertex = GeometryGenerator::Vertex;
+
+MeshData GeometryGenerator::CreateBox(float width, float height, float depth, uint32 numSubdivisions)
+{
+	MeshData meshData;
+
+	Vertex v[24];
+	float w2 = 0.5f*width;
+	float h2 = 0.5f*height;
+	float d2 = 0.5f*depth;
+	
+	// 前面
+	v[0] = Vertex(-w2, -h2, -d2, 0.f, 0.f, -1.f, 1.f, 0.f, 0.f, 0.f, 1.f);
+	v[1] = Vertex(-w2, +h2, -d2, 0.f, 0.f, -1.f, 1.f, 0.f, 0.f, 0.f, 0.f);
+	v[2] = Vertex(+w2, +h2, -d2, 0.f, 0.f, -1.f, 1.f, 0.f, 0.f, 1.f, 0.f);
+	v[3] = Vertex(+w2, -h2, -d2, 0.f, 0.f, -1.f, 1.f, 0.f, 0.f, 1.f, 1.f);
+	// 后面
+	v[4] = Vertex(-w2, -h2, +d2, 0.f, 0.f, 1.f, -1.f, 0.f, 0.f, 1.f, 1.f);
+	v[5] = Vertex(+w2, -h2, +d2, 0.f, 0.f, 1.f, -1.f, 0.f, 0.f, 0.f, 1.f);
+	v[6] = Vertex(+w2, +h2, +d2, 0.f, 0.f, 1.f, -1.f, 0.f, 0.f, 1.f, 0.f);
+	v[7] = Vertex(-w2, +h2, +d2, 0.f, 0.f, 1.f, -1.f, 0.f, 0.f, 1.f, 0.f);
+	// top
+	v[8] = Vertex(-w2, +h2, -d2, 0.f, 1.f, 0.f, 1.f, 0.f, 0.f, 0.f, 1.f);
+	v[9] = Vertex(-w2, +h2, +d2, 0.f, 1.f, 0.f, 1.f, 0.f, 0.f, 0.f, 0.f);
+	v[10] = Vertex(+w2, +h2, +d2, 0.f, 1.f, 0.f, 1.f, 0.f, 0.f, 1.f, 0.f);
+	v[11] = Vertex(+w2, +h2, -d2, 0.f, 1.f, 0.f, 1.f, 0.f, 0.f, 1.f, 1.f);
+	// Fill in the bottom face vertex data.
+	v[12] = Vertex(-w2, -h2, -d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+	v[13] = Vertex(+w2, -h2, -d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[14] = Vertex(+w2, -h2, +d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[15] = Vertex(-w2, -h2, +d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	// Fill in the left face vertex data.
+	v[16] = Vertex(-w2, -h2, +d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[17] = Vertex(-w2, +h2, +d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[18] = Vertex(-w2, +h2, -d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+	v[19] = Vertex(-w2, -h2, -d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+
+	// Fill in the right face vertex data.
+	v[20] = Vertex(+w2, -h2, -d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f);
+	v[21] = Vertex(+w2, +h2, -d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+	v[22] = Vertex(+w2, +h2, +d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
+	v[23] = Vertex(+w2, -h2, +d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+
+
+	meshData.Vertices.assign(&v[0], &v[24]);
+
+	uint32 i[36];
+	
+	// Fill in the front face index data
+	i[0] = 0; i[1] = 1; i[2] = 2;
+	i[3] = 0; i[4] = 2; i[5] = 3;
+
+	// Fill in the back face index data
+	i[6] = 4; i[7] = 5; i[8] = 6;
+	i[9] = 4; i[10] = 6; i[11] = 7;
+
+	// Fill in the top face index data
+	i[12] = 8; i[13] = 9; i[14] = 10;
+	i[15] = 8; i[16] = 10; i[17] = 11;
+
+	// Fill in the bottom face index data
+	i[18] = 12; i[19] = 13; i[20] = 14;
+	i[21] = 12; i[22] = 14; i[23] = 15;
+
+	// Fill in the left face index data
+	i[24] = 16; i[25] = 17; i[26] = 18;
+	i[27] = 16; i[28] = 18; i[29] = 19;
+
+	// Fill in the right face index data
+	i[30] = 20; i[31] = 21; i[32] = 22;
+	i[33] = 20; i[34] = 22; i[35] = 23;
+
+	meshData.Indices32.assign(&i[0], &i[36]);
+
+	numSubdivisions = min<uint32>(numSubdivisions, 6u);
+	for (uint32 i = 0; i < numSubdivisions; ++i)
+	{
+		Subdivide(meshData);
+	}
+	return meshData;
+}
+
+MeshData GeometryGenerator::CreateSphere(float radius, uint32 sliceCount, uint32 stackCount)
+{
+	MeshData meshData;
+
+	Vertex topVertex(0.f, +radius, 0.f, 0.f, 1.f, 0.f, 0.f, 1.f, 0.f, 0.f, 0.f);
+	Vertex bottomVertex(0.f, -radius, 0.f, 0.f, 0.f, -1.f, 0.f, 1.f, 0.f, 0.f, 1.f);
+
+	meshData.Vertices.push_back(topVertex);
+
+	return meshData;
+}
+
+MeshData GeometryGenerator::CreateGeosphere(float radius, uint32 numSubdivisions)
+{
+	return MeshData();
+}
+
+MeshData GeometryGenerator::CreateCylinder(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount)
+{
+	return MeshData();
+}
+
+MeshData GeometryGenerator::CreateGrid(float width, float depth, uint32 m, uint32 n)
+{
+	return MeshData();
+}
+
+MeshData GeometryGenerator::CreateQuad(float x, float y, float w, float h, float depth)
+{
+	return MeshData();
+}
+
+void GeometryGenerator::Subdivide(MeshData & meshData)
+{
+}
+
+Vertex GeometryGenerator::MidPoint(const Vertex & v0, const Vertex & v1)
+{
+	return Vertex();
+}
+
+void GeometryGenerator::BuildCylinderTopCap(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount, MeshData & meshData)
+{
+}
+
+void GeometryGenerator::BuildCylinderBottomCap(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount, MeshData & meshData)
+{
+}
